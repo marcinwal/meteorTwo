1356a7ced1fc85b084d025b0695adc3300e51f70
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-21 16:10:31 +0100

    working chapter 5end

--- client/clientOrigin.js
+++ client/clientOrigin.js
@@ -5,21 +5,21 @@ var newHouse = {
   lastsave: 'never',
   status: 'unsaved'
 };
+Session.setDefault('selectedHouseId', '');
 
-Session.setDefault('selectedHouseId','');
-
-Tracker.autorun(function(){
-  console.log("The selected Hose ID is: " + 
-    Session.get("selectedHouseId"));
+Tracker.autorun(function () {
+  console.log('The selectedHouse ID is: ' +
+    Session.get('selectedHouseId')
+  );
 });
 
-Template.registerHelper('selectedHouse',function(){
-  return LocalHouse.findOne(Session.get('selectedHouseId'))
+Template.registerHelper('selectedHouse', function () {
+  return LocalHouse.findOne(Session.get('selectedHouseId'));
 });
 
-Template.registerHelper('withIndex',function (list){
-  var withIndex = _.map(list,function (v,i){
-    if(v === null) return;
+Template.registerHelper('withIndex', function (list) {
+  var withIndex = _.map(list, function (v, i) {
+    if (v === null) return;
     v.index = i;
     return v;
   });
@@ -27,69 +27,69 @@ Template.registerHelper('withIndex',function (list){
 });
 
 Template.notificationArea.helpers({
-  notification: function(){
-    return Session.get("notification");
+  notification: function () {
+    return Session.get('notification');
   }
 });
 
 Template.selectHouse.helpers({
-  housesNameId: function(){
-    return HousesCollection.find({},{
+  housesNameId: function () {
+    return HousesCollection.find({}, {
       fields: {
         name: 1,
         _id: 1
       }
     });
   },
-  isSelected: function(){
-    return Session.equals('selectedHouseId',this._id)? 'selected' : '';
+  isSelected: function () {
+    return Session.equals('selectedHouseId', this._id) ? 'selected' : '';
   }
 });
-Template.selectHouse.events = {
-  'change #selectHouse': function(evt){
+Template.selectHouse.events({
+  'change #selectHouse': function (evt) {
     var selectedId = evt.currentTarget.value;
     var newId = LocalHouse.upsert(
       selectedId,
       HousesCollection.findOne(selectedId) || newHouse
-      ).insertedId;  //insertedId is returned
-    if(!newId) newId = selectedId; //difference!!
-    Session.set("selectedHouseId",newId);
+    ).insertedId;
+    if (!newId) newId = selectedId;
+    Session.set('selectedHouseId', newId);
   }
-};
+});
 
 Template.showHouse.events({
-  'click button#delete': function(evt){
-    var house = this;
+  'click button#delete': function (evt) {
     var id = this._id;
-    var deleteConfirmation = confirm("Are you sure?");
-    if (deleteConfirmation){
+    var deleteConfirmation = confirm('Really delete this house?');
+    if (deleteConfirmation) {
+      // remove from remote db
       HousesCollection.remove(id);
+      // remove from local collection
       LocalHouse.remove(id);
     }
   }
 });
 
-
 Template.plantDetails.helpers({
-  isWatered: function(){
-    var plantId = Session.get("selectedHouseId")+"-"+this.color;
-    return Session.get(plantId)? 'disabled' : '';
+  isWatered: function () {
+    var plantId = Session.get('selectedHouseId') + '-' + this.color;
+    return Session.get(plantId) ? 'disabled' : '';
   }
 });
 Template.plantDetails.events({
-  'click button.water': function(evt){
-    var lastvisit = new Date();
+  'click button.water': function (evt) {
     var plantId = $(evt.currentTarget).attr('data-id');
-    Session.set(plantId,true);
+    Session.set(plantId, true);
+    var lastvisit = new Date();
     HousesCollection.update({
-      _id:Session.get("selectedHouseId")
-    },{
-      $set:{lastvisit: lastvisit
+      _id: Session.get('selectedHouseId')
+    }, {
+      $set: {
+        lastvisit: lastvisit
       }
     });
   }
 });
-
 
 Template.houseForm.onCreated(function () {
   this.autorun(function () {
@@ -111,7 +111,7 @@ Template.houseForm.onCreated(function () {
   })
 });
 Template.houseForm.events({
-  'keyup input#house-name': function(evt){
+  'keyup input#house-name': function (evt) {
     evt.preventDefault();
     var modifier = {
       $set: {
@@ -119,9 +119,9 @@ Template.houseForm.events({
         'status': 'unsaved'
       }
     };
-    updateLocalHouse(Session.get('selectedHouseId'),modifier);
-  },  
-  'click button.addPlant': function(evt){
+    updateLocalHouse(Session.get('selectedHouseId'), modifier);
+  },
+  'click button.addPlant': function (evt) {
     evt.preventDefault();
     var newPlant = {
       color: '',
@@ -131,13 +131,13 @@ Template.houseForm.events({
       $push: {
         'plants': newPlant
       },
-      $set:{
-        'status':'unsaved'
+      $set: {
+        'status': 'unsaved'
       }
     };
-    updateLocalHouse(Session.get('selectedHouseId'),modifier);
+    updateLocalHouse(Session.get('selectedHouseId'), modifier);
   },
-  'click button#save-house': function(evt){
+  'click button#save-house': function (evt) {
     evt.preventDefault();
     var id = Session.get('selectedHouseId');
     var modifier = {
@@ -146,50 +146,48 @@ Template.houseForm.events({
         'status': 'saved'
       }
     };
-    updateLocalHouse(id,modifier);
+    updateLocalHouse(id, modifier);
+    // persist house document in remote db
     HousesCollection.upsert({
-      _id: id
-    },
+        _id: id
+      },
       LocalHouse.findOne(id)
     );
   }
 });
 
-
 Template.plantFieldset.events({
-  'click button.removePlant': function(evt){
+  'click button.removePlant': function (evt) {
     evt.preventDefault();
     var index = evt.target.getAttribute('data-index');
-    var plants = Template.parentData(1).plants; //accessing for 1 
-    //template parrent above
-    plants.splice(index,1);
+    var plants = Template.parentData(1).plants;
+    plants.splice(index, 1);
     var modifier = {
-      $set:{
+      $set: {
         'plants': plants,
-        'status':'unsaved'
+        'status': 'unsaved'
       }
     };
-    updateLocalHouse(Session.get('selectedHouseId'),modifier);
+    updateLocalHouse(Session.get('selectedHouseId'), modifier);
   },
-  'keyup input.color, keyup input.instructions': function(evt){
+  'keyup input.color, keyup input.instructions': function (evt) {
     evt.preventDefault();
     var index = evt.target.getAttribute('data-index');
     var field = evt.target.getAttribute('class');
-    var plantProperty = 'plants.'+index+'.'+field;
+    var plantProperty = 'plants.' + index + '.' + field;
     var modifier = {
       $set: {}
     };
-    modifier['$set'][plantProperty] = evt.target.value; //???????
-    //aasigning a new value using brucker notation
+    modifier['$set'][plantProperty] = evt.target.value;
     modifier['$set'].status = 'unsaved';
-    updateLocalHouse(Session.get('selectedHouseId'),modifier);
+
+    updateLocalHouse(Session.get('selectedHouseId'), modifier);
   }
 });
 
-updateLocalHouse = function (id,modifier){
-  LocalHouse.update(
-    {
-      '_id': id,
+updateLocalHouse = function (id, modifier) {
+  LocalHouse.update({
+      '_id': id
     },
     modifier
   );
